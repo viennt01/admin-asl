e3fdeaaa44a9b696f9b5fa866f4fc669150b8f55
viennt01 <thanhviennguyen01@gmail.com>
2023-05-11 09:36:27 +0700

    add page login

---
+++ src/components/login-page/fetcher.ts
@@ -0,0 +1,13 @@
+import { post, ResponseWithPayload } from '@/fetcher';
+import { API_AUTHENTICATE } from '@/fetcher/endpoint';
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export const login = (data: LoginData) => {
+  return post<LoginData, ResponseWithPayload<string>>({ data })(
+    API_AUTHENTICATE.LOGIN
+  );
+};
