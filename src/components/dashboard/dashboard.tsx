38c05852cfe05be6e78497e0cbe618de942d148d
viennt01 <thanhviennguyen01@gmail.com>
2023-05-12 23:50:50 +0700

    layout dashboard

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,24 +1,95 @@
-import { Breadcrumb, Card } from 'antd';
+import { ProCard, StatisticCard } from '@ant-design/pro-components';
+import { Breadcrumb } from 'antd';
+import RcResizeObserver from 'rc-resize-observer';
+import { useState } from 'react';
+
+const { Statistic } = StatisticCard;
 
 export default function DashboardPage() {
+  const [responsive, setResponsive] = useState(false);
+  const today = new Date();
   return (
-    <>
+    <div style={{ margin: '32px 0' }}>
       <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+        <Breadcrumb.Item>/Dashboard</Breadcrumb.Item>
       </Breadcrumb>
-
-      <Card
-        bordered={false}
-        style={{
-          width: 'fit-content',
-          margin: '0 0 24px auto',
-        }}
-        bodyStyle={{
-          padding: 12,
+      <RcResizeObserver
+        key="resize-observer"
+        onResize={(offset) => {
+          setResponsive(offset.width < 596);
         }}
       >
-        <h1>Dashboard</h1>
-      </Card>
-    </>
+        <ProCard
+          title="Dashboard"
+          extra={today.toLocaleDateString('vi-VN')}
+          split={responsive ? 'horizontal' : 'vertical'}
+          headerBordered
+          bordered
+        >
+          <ProCard split="horizontal">
+            <ProCard split="horizontal">
+              <ProCard split="vertical">
+                <StatisticCard
+                  statistic={{
+                    title: 'Tổng doanh thu tháng',
+                    value: '234,123,1223',
+                    description: (
+                      <Statistic
+                        title="Tăng trưởng"
+                        value="8.04%"
+                        trend="down"
+                      />
+                    ),
+                  }}
+                />
+                <StatisticCard
+                  statistic={{
+                    title: 'Tổng doanh thu năm',
+                    value: '234,123,1223',
+                    description: (
+                      <Statistic title="Tăng trưởng" value="8.04%" trend="up" />
+                    ),
+                  }}
+                />
+              </ProCard>
+              <ProCard split="vertical">
+                <StatisticCard
+                  statistic={{
+                    title: 'Tổng số container hiện có',
+                    value: '1256',
+                  }}
+                />
+                <StatisticCard
+                  statistic={{
+                    title: 'Tổng số container rỗng',
+                    value: '134',
+                  }}
+                />
+              </ProCard>
+            </ProCard>
+            <StatisticCard
+              title="PNL"
+              chart={
+                <img
+                  src="https://gw.alipayobjects.com/zos/alicdn/_dZIob2NB/zhuzhuangtu.svg"
+                  width="100%"
+                  alt="chart"
+                />
+              }
+            />
+          </ProCard>
+          <StatisticCard
+            title="Bình quân lợi nhuận"
+            chart={
+              <img
+                src="https://gw.alipayobjects.com/zos/alicdn/qoYmFMxWY/jieping2021-03-29%252520xiawu4.32.34.png"
+                alt="profit"
+                width="100%"
+              />
+            }
+          />
+        </ProCard>
+      </RcResizeObserver>
+    </div>
   );
 }
