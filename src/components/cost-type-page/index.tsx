ba922a8a551dcdf8b1987a8dd6f89be62accdb13
Nguyen Cao Ngoc Ngan <ngocngan060701@gmail.com>
2023-05-17 22:52:48 +0700

    feat/ChangeLanguagesAndMarginBottomInLink

--- src/components/cost-type-page/index.tsx
+++ src/components/cost-type-page/index.tsx
@@ -3,7 +3,7 @@ import { Breadcrumb } from 'antd';
 export default function CostTypePage() {
   return (
     <>
-      <Breadcrumb style={{ margin: '16px 0' }}>
+      <Breadcrumb style={{ margin: '15px 0' }}>
         <Breadcrumb.Item>COST TYPE</Breadcrumb.Item>
       </Breadcrumb>
     </>
