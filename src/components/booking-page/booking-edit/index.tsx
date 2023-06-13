4afe3818e99b4178f65032867fcc73470c7a1a17
viennt01 <thanhviennguyen01@gmail.com>
2023-06-13 23:41:29 +0700

    feat/ ui booking

--- src/components/booking-page/booking-edit/index.tsx
+++ src/components/booking-page/booking-edit/index.tsx
@@ -1,43 +1,404 @@
-import React from 'react';
-import { Badge, Card, Descriptions } from 'antd';
+import COLORS from '@/constant/color';
+import { ROUTERS } from '@/constant/router';
 import {
-  UserOutlined,
-  MailOutlined,
-  PhoneOutlined,
-  HomeOutlined,
-} from '@ant-design/icons';
+  Button,
+  Form,
+  Input,
+  Typography,
+  Card,
+  Row,
+  Col,
+  ConfigProvider,
+  Select,
+  Cascader,
+  CascaderProps,
+  DatePicker,
+  Table,
+} from 'antd';
+import { ColumnsType } from 'antd/es/table';
+import dayjs from 'dayjs';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+interface DataNodeType {
+  value: string;
+  label: string;
+  children?: DataNodeType[];
+}
+export interface FormValues {
+  type_of_container: string;
+  detail_placeholder: string;
+  number_container: string;
+}
+interface DataType {
+  key: React.Key;
+  containerNo: string;
+  typeContainer: string;
+  status: string;
+}
+
+const initialValue = {
+  type_of_container: '',
+  detail_placeholder: '',
+  number_container: '',
+};
+
+const { Title } = Typography;
 
 export default function EditBooking() {
-  return (
-    <Card bordered={false} style={{ margin: '10px 0' }}>
-      <Descriptions title="THÔNG TIN CHI TIẾT CỦA BOOKING" column={2}>
-        <Descriptions.Item label={<UserOutlined />}>
-          Công ty Cảng quốc tế SP-PSA
-        </Descriptions.Item>
-        <Descriptions.Item label={<MailOutlined />}>
-          thanhviennguyen01@gmail.com
-        </Descriptions.Item>
-        <Descriptions.Item label={<PhoneOutlined />}>
-          1810000000
-        </Descriptions.Item>
-        <Descriptions.Item label={<HomeOutlined />}>
-          Thành phố Hồ Chí Minh
-        </Descriptions.Item>
-      </Descriptions>
-      <hr style={{ width: '70%', marginBottom: '24px' }} color="#BBBBBB" />
+  const router = useRouter();
+  const [form] = Form.useForm<FormValues>();
+  const { id } = router.query;
+  const dateFormat = 'YYYY/MM/DD';
 
-      <Descriptions column={2}>
-        <Descriptions.Item label="Số lượng container">
-          100.000.000{' '}
-        </Descriptions.Item>
-        <Descriptions.Item label="Sức chứa">181</Descriptions.Item>
-        <Descriptions.Item label="Trạng thái">
-          <Badge status="processing" text="Hoạt động bình thường" />
-        </Descriptions.Item>
-        <Descriptions.Item label="Trạng thái sức chứa">
-          <Badge status="error" text="Đầy" />
-        </Descriptions.Item>
-      </Descriptions>
-    </Card>
+  useEffect(() => {
+    if (!id) return;
+  }, [router, form]);
+
+  const onFinish = (formValues: FormValues) => {
+    console.log(formValues);
+  };
+  const residences: CascaderProps<DataNodeType>['options'] = [
+    {
+      value: 'Thành phố Hồ Chí Minh',
+      label: 'Thành phố Hồ Chí Minh',
+      children: [
+        {
+          value: 'Gò Vấp',
+          label: 'Gò Vấp',
+          children: [
+            {
+              value: 'Phường 1',
+              label: 'Phường 1',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      value: 'Hà Nội',
+      label: 'Hà Nội',
+      children: [
+        {
+          value: 'Huyện Ba Vì',
+          label: 'Huyện Ba Vì',
+          children: [
+            {
+              value: 'Xã Ba Trại',
+              label: 'Xã Ba Trại',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+  const data = [
+    {
+      key: '1',
+      containerNo: 'GMDU 307 307 9',
+      typeContainer: '40DC',
+      status: 'Đang cho thuê',
+    },
+    {
+      key: '2',
+      containerNo: 'GMDU 307 307 9',
+      typeContainer: '40HC',
+      status: 'Yêu cầu vệ sinh',
+    },
+    {
+      key: '3',
+      containerNo: 'GMDU 307 307 9',
+      typeContainer: '40DC',
+      status: 'Đang cho thuê',
+    },
+    {
+      key: '4',
+      containerNo: 'GMDU 307 307 9',
+      typeContainer: '40HC',
+      status: 'Yêu cầu vệ sinh',
+    },
+  ];
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Mã số container',
+      dataIndex: 'containerNo',
+      align: 'center',
+    },
+    {
+      title: 'Loại container',
+      dataIndex: 'typeContainer',
+      align: 'center',
+    },
+    {
+      title: 'Tình trạng container',
+      dataIndex: 'status',
+      align: 'center',
+    },
+  ];
+  return (
+    <div style={{ padding: '24px 0' }}>
+      <Card style={{ marginBottom: 24 }}>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: COLORS.GREEN,
+            },
+          }}
+        >
+          <Form
+            form={form}
+            initialValues={initialValue}
+            onFinish={onFinish}
+            autoComplete="off"
+            layout="vertical"
+          >
+            <Row justify={'center'}>
+              <Col>
+                <Title level={3}>Edit a booking</Title>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Mã booking"
+                  name="code_booking"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input type of mã booking',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Nhập mã booking" />
+                </Form.Item>
+              </Col>
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Package"
+                  name="package"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input package',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Nhập package" />
+                </Form.Item>
+              </Col>
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Cảng chất hàng"
+                  name="port_of_loading"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input type of container',
+                    },
+                  ]}
+                >
+                  <Select
+                    options={[
+                      {
+                        value: 'Hải Phòng',
+                        label: 'Hải Phòng',
+                      },
+                      {
+                        value: 'Hồ Chí Minh',
+                        label: 'Hồ Chí Minh',
+                      },
+                    ]}
+                  />
+                </Form.Item>
+              </Col>
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Cảng dỡ hàng"
+                  name="port_of_discharge"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input type of container',
+                    },
+                  ]}
+                >
+                  <Select
+                    options={[
+                      {
+                        value: 'Hải Phòng',
+                        label: 'Hải Phòng',
+                      },
+                      {
+                        value: 'Hồ Chí Minh',
+                        label: 'Hồ Chí Minh',
+                      },
+                    ]}
+                  />
+                </Form.Item>
+              </Col>
+
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Địa điểm giao hàng"
+                  name="location"
+                  rules={[
+                    { required: true, message: 'Please input last name' },
+                  ]}
+                >
+                  <Cascader options={residences} />
+                </Form.Item>
+              </Col>
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Địa điểm giao hàng cụ thể"
+                  name="detail_location"
+                  rules={[
+                    { required: true, message: 'Please input last name' },
+                  ]}
+                >
+                  <Input placeholder="Nhập vị trí cụ thể" />
+                </Form.Item>
+              </Col>
+
+              <Col lg={8} span={24}>
+                <Form.Item
+                  label="ETD"
+                  name="etd"
+                  rules={[{ required: true, message: 'Please input ETD' }]}
+                >
+                  <DatePicker
+                    defaultValue={dayjs('2015/01/01', dateFormat)}
+                    format={dateFormat}
+                  />
+                </Form.Item>
+              </Col>
+              <Col lg={8} span={24}>
+                <Form.Item
+                  label="ETA"
+                  name="eta"
+                  rules={[{ required: true, message: 'Please input ETA' }]}
+                >
+                  <DatePicker
+                    defaultValue={dayjs('2015/01/01', dateFormat)}
+                    format={dateFormat}
+                  />
+                </Form.Item>
+              </Col>
+              <Col lg={8} span={24}>
+                <Form.Item
+                  label="Date created"
+                  name="date_created"
+                  rules={[
+                    { required: true, message: 'Please input date created' },
+                  ]}
+                >
+                  <DatePicker
+                    defaultValue={dayjs('2015/01/01', dateFormat)}
+                    format={dateFormat}
+                  />
+                </Form.Item>
+              </Col>
+
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Creator"
+                  name="creator"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input type of Creator',
+                    },
+                  ]}
+                >
+                  <Select
+                    options={[
+                      {
+                        value: 'Ngân',
+                        label: 'Ngân',
+                      },
+                      {
+                        value: 'Khoa',
+                        label: 'Khoa',
+                      },
+                    ]}
+                  />
+                </Form.Item>
+              </Col>
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Status"
+                  name="status"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input type of status',
+                    },
+                  ]}
+                >
+                  <Select
+                    options={[
+                      {
+                        value: 'Save As Draft',
+                        label: 'Save As Draft',
+                      },
+                      {
+                        value: 'Pending',
+                        label: 'Pending',
+                      },
+                      {
+                        value: 'Approved',
+                        label: 'Approved',
+                      },
+                      {
+                        value: 'Processing',
+                        label: 'Processing',
+                      },
+                      {
+                        value: 'Completed',
+                        label: 'Completed',
+                      },
+                      {
+                        value: 'Cancel',
+                        label: 'Cancel',
+                      },
+                    ]}
+                  />
+                </Form.Item>
+              </Col>
+
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Note"
+                  name="note"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input note',
+                    },
+                  ]}
+                >
+                  <Input.TextArea placeholder="Nhập ghi chú" />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Card style={{ marginBottom: 24 }} title="Danh sách container">
+              <Table columns={columns} dataSource={data} />
+            </Card>
+            <Row gutter={12}>
+              <Col>
+                <Button onClick={() => router.push(ROUTERS.TYPES_OF_CONTAINER)}>
+                  Cancel
+                </Button>
+              </Col>
+              <Col>
+                <Button type="primary" htmlType="submit">
+                  Save
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </ConfigProvider>
+      </Card>
+    </div>
   );
 }
