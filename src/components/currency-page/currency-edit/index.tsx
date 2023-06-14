730ed9dc5b7a20101ce208f536310687c6cca858
viennt01 <thanhviennguyen01@gmail.com>
2023-06-14 17:31:42 +0700

    feat/ ui edit currency

--- src/components/currency-page/currency-edit/index.tsx
+++ src/components/currency-page/currency-edit/index.tsx
@@ -1,43 +1,238 @@
-import React from 'react';
-import { Badge, Card, Descriptions } from 'antd';
+import COLORS from '@/constant/color';
+import { ROUTERS } from '@/constant/router';
 import {
-  UserOutlined,
-  MailOutlined,
-  PhoneOutlined,
-  HomeOutlined,
-} from '@ant-design/icons';
+  Button,
+  Form,
+  Input,
+  Typography,
+  Card,
+  Row,
+  Col,
+  ConfigProvider,
+  Select,
+} from 'antd';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export interface FormValues {
+  type_of_container: string;
+  detail_placeholder: string;
+  number_container: string;
+}
+
+const initialValue = {
+  type_of_container: '',
+  detail_placeholder: '',
+  number_container: '',
+};
+
+const { Title } = Typography;
 
 export default function EditCurrency() {
-  return (
-    <Card bordered={false} style={{ margin: '10px 0' }}>
-      <Descriptions title="THÔNG TIN CHI TIẾT CỦA TỶ GIÁ TIỀN TỆ" column={2}>
-        <Descriptions.Item label={<UserOutlined />}>
-          Công ty Cảng quốc tế SP-PSA
-        </Descriptions.Item>
-        <Descriptions.Item label={<MailOutlined />}>
-          thanhviennguyen01@gmail.com
-        </Descriptions.Item>
-        <Descriptions.Item label={<PhoneOutlined />}>
-          1810000000
-        </Descriptions.Item>
-        <Descriptions.Item label={<HomeOutlined />}>
-          Thành phố Hồ Chí Minh
-        </Descriptions.Item>
-      </Descriptions>
-      <hr style={{ width: '70%', marginBottom: '24px' }} color="#BBBBBB" />
+  const router = useRouter();
+  const [form] = Form.useForm<FormValues>();
+  const { id } = router.query;
 
-      <Descriptions column={2}>
-        <Descriptions.Item label="Số lượng container">
-          100.000.000{' '}
-        </Descriptions.Item>
-        <Descriptions.Item label="Sức chứa">181</Descriptions.Item>
-        <Descriptions.Item label="Trạng thái">
-          <Badge status="processing" text="Hoạt động bình thường" />
-        </Descriptions.Item>
-        <Descriptions.Item label="Trạng thái sức chứa">
-          <Badge status="error" text="Đầy" />
-        </Descriptions.Item>
-      </Descriptions>
-    </Card>
+  useEffect(() => {
+    if (!id) return;
+  }, [router, form]);
+
+  const onFinish = (formValues: FormValues) => {
+    console.log(formValues);
+  };
+
+  return (
+    <div style={{ padding: '24px 0' }}>
+      <Card style={{ marginBottom: 24 }}>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: COLORS.GREEN,
+            },
+          }}
+        >
+          <Form
+            form={form}
+            initialValues={initialValue}
+            onFinish={onFinish}
+            autoComplete="off"
+            layout="vertical"
+          >
+            <Row justify={'center'}>
+              <Col>
+                <Title level={3}>Edit a currency</Title>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Currency From"
+                  name="currency_from"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input type of Currency From',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Nhập mã Currency From" />
+                </Form.Item>
+              </Col>
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Currency To"
+                  name="currency-to"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input Currency To',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Nhập Currency To" />
+                </Form.Item>
+              </Col>
+
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Exchange rate"
+                  name="exchange_rate"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input Exchange rate',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Nhập Exchange rate" />
+                </Form.Item>
+              </Col>
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Bank"
+                  name="bank"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input Bank',
+                    },
+                  ]}
+                >
+                  <Select
+                    options={[
+                      {
+                        value: 'BIDV',
+                        label: 'BIDV',
+                      },
+                      {
+                        value: 'VCB',
+                        label: 'VCB',
+                      },
+                    ]}
+                  />
+                </Form.Item>
+              </Col>
+
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Cash (buy)"
+                  name="cash_buy"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input type of Cash (buy)',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Nhập Cash (buy)" />
+                </Form.Item>
+              </Col>
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Cash (sell)"
+                  name="cash_sell"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input type of Cash (sell)',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Nhập Cash (sell)" />
+                </Form.Item>
+              </Col>
+
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Transfer (buy)"
+                  name="transfer_buy"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input type of Transfer (buy)',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Nhập Transfer (buy)" />
+                </Form.Item>
+              </Col>
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Transfer (sell)"
+                  name="transfer_sell"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input type of Transfer (sell)',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Nhập Transfer (sell)" />
+                </Form.Item>
+              </Col>
+
+              <Col lg={12} span={24}>
+                <Form.Item
+                  label="Status"
+                  name="status"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input type of status',
+                    },
+                  ]}
+                >
+                  <Select
+                    options={[
+                      {
+                        value: 'Increase',
+                        label: 'Increase',
+                      },
+                      {
+                        value: 'Decrease',
+                        label: 'Decrease',
+                      },
+                    ]}
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Row gutter={12}>
+              <Col>
+                <Button onClick={() => router.push(ROUTERS.CURRENCY)}>
+                  Cancel
+                </Button>
+              </Col>
+              <Col>
+                <Button type="primary" htmlType="submit">
+                  Save
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </ConfigProvider>
+      </Card>
+    </div>
   );
 }
