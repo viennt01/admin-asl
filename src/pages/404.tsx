aa37693e1c075bf2250c77ef8b0984424e1eda23
viennt01 <thanhviennguyen01@gmail.com>
2023-08-20 01:14:19 +0700

    fix/ 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,24 +3,13 @@ import { Button, Result } from 'antd';
 import { useRouter } from 'next/router';
 import { ROUTERS } from '@/constant/router';
 import useI18n from '@/i18n/useI18N';
-import { AppLayout } from '@/layout/authen-layout';
-import { PageWithNoLayout } from '@/layout/no-layout';
-import { LOCAL_STORAGE_KEYS } from '@/constant/localstorage';
-import { appLocalStorage } from '@/utils/localstorage';
-function Error() {
+
+const Error: React.FC = () => {
   const { translate: translateCommon } = useI18n('common');
   const router = useRouter();
   const handleChangePage = () => {
     router.push(ROUTERS.HOME);
   };
-  const isServer = typeof window === 'undefined';
-  if (!isServer) {
-    const checkToken = appLocalStorage.get(LOCAL_STORAGE_KEYS.TOKEN);
-    if (!checkToken) {
-      router.push(ROUTERS.LOGIN);
-      return;
-    }
-  }
   return (
     <Result
       status="404"
@@ -31,22 +20,9 @@ function Error() {
           {translateCommon('button_back_home')}
         </Button>
       }
-      style={{ backgroundColor: '#fff', height: '100vh' }}
     />
   );
-}
-
-const isServer = typeof window === 'undefined';
-if (!isServer) {
-  const checkToken = appLocalStorage.get(LOCAL_STORAGE_KEYS.TOKEN);
-  Error.getLayout = () => {
-    if (!checkToken) {
-      return PageWithNoLayout;
-    } else {
-      return AppLayout;
-    }
-  };
-}
+};
 
 export default Error;
 
