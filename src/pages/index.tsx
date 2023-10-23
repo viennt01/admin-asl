b9b1db1e411451ac206c5ce56db88e01c922b55e
Nguyen Cao Ngoc Ngan <ngocngan060701@gmail.com>
2023-10-23 16:20:09 +0700

    feat/RegisterV2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import withAuthentication from '@/hook/useAuthentication';
 import { PageWithNoLayout } from '@/layout/no-layout';
-import L from '@/components/login';
-// import L from '@/components/login-v2';
+//import L from '@/components/login';
+import L from '@/components/login-v2';
 import { Inter } from '@next/font/google';
 import Head from 'next/head';
 
@@ -18,14 +18,14 @@ function Login() {
     </>
   );
 }
-const LoginPage = withAuthentication(Login);
-// const LoginPageV2 = withAuthentication(Login);
+//const LoginPage = withAuthentication(Login);
+const LoginPageV2 = withAuthentication(Login);
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 
-LoginPage.Layout = PageWithNoLayout;
-// LoginPageV2.Layout = PageWithNoLayout;
+//LoginPage.Layout = PageWithNoLayout;
+LoginPageV2.Layout = PageWithNoLayout;
 
-export default LoginPage;
-// export default LoginPageV2;
+//export default LoginPage;
+export default LoginPageV2;
